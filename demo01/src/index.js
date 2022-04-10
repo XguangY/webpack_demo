9e91381f3d8ec105887bdf38176be788530d0911
XguangY <1095772603@qq.com>
2022-04-10 18:30:08 +0800

    demo 01

---
+++ demo01/src/index.js
@@ -0,0 +1,14 @@
+// 插件式引入lodash
+import _ from "lodash";
+
+function component() {
+  var element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack666666"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
